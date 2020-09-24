2139bec47feae66278084d720e613a3f4c3d5ffc
Djura22 <michaeliangroves@gmail.com>
2020-09-24 11:23:02 +0100

    datafillerservice added

--- src/main/java/com/michael/developer/postgresql/entities/Course.java
+++ src/main/java/com/michael/developer/postgresql/entities/Course.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.ForeignKey;
 
 @Entity
 @Data
